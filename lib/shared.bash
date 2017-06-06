749da28cf45b84fb3f66281cc809d3c009d20762
Lachlan Donald <lachlan@ljd.cc>
2017-06-06 15:52:25 +1000

    Reduce the headers in the output

--- lib/shared.bash
+++ lib/shared.bash
@@ -53,11 +53,11 @@ add_ssh_private_key_to_agent() {
   local ssh_key="$1"
 
   if [[ -z "${SSH_AGENT_PID:-}" ]] ; then
-    echo "~~~ Starting an ephemeral ssh-agent" >&2;
+    echo "Starting an ephemeral ssh-agent" >&2;
     eval "$(ssh-agent -s)"
   fi
 
-  echo "~~~ Loading ssh-key into ssh-agent (pid ${SSH_AGENT_PID:-})" >&2;
+  echo "Loading ssh-key into ssh-agent (pid ${SSH_AGENT_PID:-})" >&2;
   echo "$ssh_key" | env SSH_ASKPASS="/bin/false" ssh-add -
 }
 
