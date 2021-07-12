90deee4f84d2bf41d8b46ac439ad934605f557f3
Keith Duncan <keith.d@buildkite.com>
2021-07-12 17:31:44 +1000

    Use on-demand for the final credential step

--- .buildkite/steps/github-release.sh
+++ .buildkite/steps/github-release.sh
@@ -12,15 +12,6 @@ then
 	exit 1
 fi
 
-echo '--- Getting credentials from SSM'
-export GITHUB_RELEASE_ACCESS_TOKEN=$(aws ssm get-parameter --name /pipelines/elastic-ci-stack-s3-secrets-hooks/GITHUB_RELEASE_ACCESS_TOKEN --with-decryption --output text --query Parameter.Value --region us-east-1)
-
-if [ -z "${GITHUB_RELEASE_ACCESS_TOKEN}" ]
-then
-  echo "Error: Missing \$GITHUB_RELEASE_ACCESS_TOKEN, set AWS SSM /pipelines/elastic-ci-stack-s3-secrets-hooks/GITHUB_RELEASE_ACCESS_TOKEN in us-east-1 before invoking this step"
-  exit 1
-fi
-
 echo '--- Downloading releases'
 
 rm -rf pkg
@@ -29,6 +20,4 @@ buildkite-agent artifact download "pkg/*"
 
 echo '--- Creating GitHub Release'
 
-export GITHUB_RELEASE_REPOSITORY="buildkite/elastic-ci-stack-s3-secrets-hooks"
-
-github-release "${VERSION}" pkg/* --commit "$(git rev-parse HEAD)"
+github-release "${VERSION}" pkg/* --commit "$(git rev-parse HEAD)" --github-repository "buildkite/elastic-ci-stack-s3-secrets-hooks"
