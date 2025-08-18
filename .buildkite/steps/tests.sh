e6b582db6e69b96699a56d40a14b5190f8c31ac1
Joe Coleman <joe@buildkite.com>
2025-08-18 17:14:36 +0100

    Updating Go tests, adding plugin tests

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -10,13 +10,10 @@ echo arch is "$(uname -m)"
 
 go install gotest.tools/gotestsum@v1.8.0
 
-mkdir -p coverage
-COVERAGE_DIR="$PWD/coverage"
-
 if [[ "$(go env GOOS)" == "windows" ]]; then
   gotestsum --junitfile="junit-${BUILDKITE_JOB_ID:-local}.xml" -- -count=1 -race ./...
 else
-  gotestsum --junitfile="junit-${BUILDKITE_JOB_ID:-local}.xml" -- -count=1 -race -cover -test.gocoverdir="${COVERAGE_DIR}" ./...
+  gotestsum --junitfile="junit-${BUILDKITE_JOB_ID:-local}.xml" -- -count=1 -race -cover ./...
 fi
 
 echo "--- Go tests completed successfully"
